0b59d5f0f32d76d1eeb0109ad01d89aa65c81585
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2020-09-28 17:29:55 -0500

    [INIT]

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'account_move_report',
+    'version': '13.0.0.1.0',
+    'author': 'Humanytek',
+    'license': 'LGPL-3',
+    'depends': [
+    ],
+    'data': [
+        # security
+        # data
+        # reports
+        # views
+    ],
+}
